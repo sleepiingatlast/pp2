304ccd823a9e1911195b2642ec18ad759e4d3476
sleepiingatlast <sleepiingatlast@mail.ru>
2025-02-13 23:05:42 +0600

    trapezoid

---
+++ week4/2math.py
@@ -0,0 +1,7 @@
+height = int(input("задайте высоту трапеции: "))
+first_base = int(input("задайте длину первого основания: "))
+second_base = int(input("задайте длину второго основания: "))
+
+area = (1/2)*(first_base + second_base)*height
+
+print(area)
